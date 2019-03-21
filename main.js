74da80688fb66706c3366cf4407640203eaf2b0b
Naruhito Kubota <n.kubota@opt.ne.jp>
2019-03-21 23:32:25 +0900

    :construction: placing triangle

--- main.js
+++ main.js
@@ -5,20 +5,21 @@ function lineToArrow(selection) {
   const lastChar = path.substr(-1, 1);
   const t = selection.items[0];
   const c = t instanceof Path || t instanceof Line;
+  const d = t instanceof Path || t instanceof Line || t instanceof Rectangle;
 
-  if(!c) {
-    console.log('パスでも線でもないよ');
+  if(!d) {
+    console.log('パスでも線でも長方形でもないよ');
     return;
     alertNoline();
     return;
   }
-  if(lastChar === 'Z') {
+  if(c && lastChar === 'Z') {
     console.log('とじてるからエラー返すよー');
     return;
     alertNoline();
     return;
   } else {
-    generateArrow();
+    generateArrow(selection, selection.items[0]);
   }
 }
 
@@ -26,8 +27,35 @@ function alertNoline() {
   // 線じゃないときのアラート
 }
 
-function generateArrow() {
-  // 三角形でも付けとけ。
+// TODO Pathの場合、座標から角度の判定
+// Pathの場合、ポイントの数を最初と最後でうまいことする。
+// Aが入ってたらどうする？
+// forEachかなんかで処理する
+// 反対側の処理
+// もとの線の太さで三角形のサイズを可変に。
+
+function generateArrow(node, items) {
+  // PathでもLineでも処理できるようにする。
+
+  let pathX = items.boundsInParent.x;
+  let pathY = items.boundsInParent.y;
+  let color;
+  if(items instanceof Rectangle) {
+    color = items.fill;
+  } else {
+    color = items.stroke;
+  }
+  const rotation = items.rotation;
+  console.log(items.pathData);
+
+  const firstTriangle = new Path();
+  firstTriangle.pathData = `M0,8 L12,0 L12,16 Z`;
+  firstTriangle.fill = new Color(color);
+  firstTriangle.rotateAround(rotation, firstTriangle.localCenterPoint)
+  node.insertionParent.addChild(firstTriangle);
+  firstTriangle.moveInParentCoordinates(pathX - 6, pathY - 8);
+
+
 }
 
 module.exports = {
